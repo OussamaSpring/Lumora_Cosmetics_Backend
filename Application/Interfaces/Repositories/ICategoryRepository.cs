f5becfe28e1c85b5f7144cbf7bdc1d1dfbda91d3
Oussama Azzouz <aoussama.dev@gmail.com>
2025-04-12 21:34:06 +0100

    small changes

--- Application/Interfaces/Repositories/ICategoryRepository.cs
+++ Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -7,9 +7,9 @@ public interface ICategoryRepository
     Task<IEnumerable<Category>> GetAllCategoriesAsync();
     Task<IEnumerable<VariantType>> GetVariantTypesForCategoryAsync(int categoryId); // this is the most important
     Task<int> CreateCategoryAsync(Category category);
-    Task UpdateCategoryAsync(Category category);
-    Task DeleteCategoryAsync(int categoryId);
-    Task AddVariantTypeAsync(VariantType variantType);
-    Task UpdateVariantTypeAsync(VariantType variantType);
-    Task DeleteVariantTypeAsync(int variantTypeId);
+    Task<bool> UpdateCategoryAsync(Category category);
+    Task<bool> DeleteCategoryAsync(int categoryId);
+    Task<short> AddVariantTypeAsync(VariantType variantType);
+    Task<bool> UpdateVariantTypeAsync(VariantType variantType);
+    Task<bool> DeleteVariantTypeAsync(int variantTypeId);
 }
