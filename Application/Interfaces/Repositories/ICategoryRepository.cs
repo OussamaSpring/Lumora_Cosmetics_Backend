ebfa9a293398b717ebbeb230371ffe08c05c01da
Oussama Azzouz <aoussama.dev@gmail.com>
2025-04-13 11:23:24 +0100

    Feature: Add category repository with modification to the category entity

--- Application/Interfaces/Repositories/ICategoryRepository.cs
+++ Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -4,12 +4,21 @@
 namespace Application.Interfaces.Repositories;
 public interface ICategoryRepository
 {
+    Task<Category?> GetCategoryByIdAsync(int categoryId);
     Task<IEnumerable<Category>> GetAllCategoriesAsync();
     Task<IEnumerable<VariantType>> GetVariantTypesForCategoryAsync(int categoryId); // this is the most important
+
+    /*
+     * The methods below are commented out because they are not used in the current implementation.
+     * They are realted to the admin functionalities.
+
+
     Task<int> CreateCategoryAsync(Category category);
     Task<bool> UpdateCategoryAsync(Category category);
     Task<bool> DeleteCategoryAsync(int categoryId);
     Task<short> AddVariantTypeAsync(VariantType variantType);
     Task<bool> UpdateVariantTypeAsync(VariantType variantType);
     Task<bool> DeleteVariantTypeAsync(int variantTypeId);
+
+    */
 }
